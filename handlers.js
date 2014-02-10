4c2d80dc4620482a63e79da378f99ac37146d65c
Leopold Spohngellert <leopoldsg94@gmail.com>
2014-02-10 18:28:06 -0500

    hello

--- handlers.js
+++ handlers.js
@@ -3,9 +3,11 @@ exports.index = function(req, res) {
 
 }
 
-exports.watch = function(req, res) {
-    var video_id = req.query.v;
-    res.render('watch', { id: video_id } , function(err, html) {
-	res.send(html);
+var watchf = function(req, res) {
+	var video_id = req.query.v;
+	res.render('watch', { id: video_id }, function(err, html) {
+		res.send(html);
 	})
 }
+
+exports.watch = watchf;
